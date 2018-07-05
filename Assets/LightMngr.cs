0046db8f2e30ecf78a9e66a42ca962870b92c69f
Mbow1 <mbow.morniass@gmail.com>
2018-07-05 18:17:55 +0100

    debut sur les fenetres

---
+++ Assets/LightMngr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class OneLightConfiguration
+{
+    public int RoomId;
+    public int FixtureId;
+}
+
+
+public class LightMngr : MonoBehaviour {
+
+    public OneLightConfiguration parameters;
+
+    // Use this for initialization
+    void Start () {
+        SmartUdpCLient.LightManagement.Init();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    int Count=0;
+
+    void OnMouseDown()
+    {
+        Count++;
+        // this object was clicked - do something
+        Debug.Log("Button click! - "+ Count);
+        if(Count%2==0)
+        { 
+            Debug.Log(string.Format("Switch On {0}-{1}", parameters.RoomId, parameters.FixtureId));
+            SmartUdpCLient.LightManagement.SwichOn(parameters.RoomId, parameters.FixtureId);
+        }
+        else
+        {
+            Debug.Log(string.Format("Switch Off {0}-{1}", parameters.RoomId, parameters.FixtureId));
+            SmartUdpCLient.LightManagement.SwichOff(parameters.RoomId, parameters.FixtureId);
+        }
+            
+
+         
+    }
+}
---
+++ Assets/Scripts/WindowsRotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsRotate : MonoBehaviour {
+    public float RotateX = 0;
+    public float RotateY = 0;
+    public float RotateZ = 0;
+
+    public bool estFrermer = false;
+
+
+
+    public float NRotateX;
+    public float NRotateY;
+    public float NRotateZ;
+
+
+    // Use this for initialization
+    void Start () {
+        Vector3 Mangle = transform.eulerAngles;
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+		if (estFrermer)
+        {
+            transform.eulerAngles = new Vector3(NRotateX, NRotateY, NRotateZ);
+        }else
+        {
+            //transform.eulerAngles = Mangle;
+        }
+	}
+}
