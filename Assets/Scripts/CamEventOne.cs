d9bd1a7ecaf8dc277a6f3d0902d0ca7724c0afb7
Mbow1 <mbow.morniass@gmail.com>
2018-07-05 14:27:38 +0100

    camera fini

---
+++ Assets/Scripts/CamEventOne.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamEventOne : MonoBehaviour {
+
+    public Camera[] cams;
+
+    public void TeslabCameraSelect()
+    {
+        cams[0].enabled = true;
+        cams[1].enabled = false;
+        cams[2].enabled = false;
+        cams[3].enabled = false;
+
+
+    }
+
+    public void LumiereCameraSelect()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = true;
+        cams[2].enabled = false;
+        cams[3].enabled = false;
+    }
+
+    public void TuringCameraSelect()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = false;
+        cams[2].enabled = true;
+        cams[3].enabled = false;
+    }
+
+    public void NobelCameraSelect()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = false;
+        cams[2].enabled = false;
+        cams[3].enabled = true;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float minX = -45.0f;
+    public float maxX = 45.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 50f;
+    public float sensY = 50f;
+
+    public float rotationY = -61.1f;
+    public float rotationX = 10.8f;
+
+    void Update()
+    {
+       if ((rotationX <= maxX && rotationX >= minX)) {
+
+              if (Input.GetKey(KeyCode.RightArrow) )
+                {
+                    rotationX += sensX * Time.deltaTime;
+                }
+
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    rotationX -= sensX * Time.deltaTime;
+                }
+        } else
+        {
+            if (rotationX >= maxX)
+            {
+                rotationX -= 0.1f;
+            } else
+            {
+                rotationX += 0.1f;
+            }
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotationY += sensY * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotationY -= sensY * Time.deltaTime;
+        }
+
+
+
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
