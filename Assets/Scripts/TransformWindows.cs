3e6179d8f8cf2d5dd7c3e260a6528cc4351312ba
Mbow1 <mbow.morniass@gmail.com>
2018-07-06 17:07:43 +0100

    Probléme de rotation sur les fenetres git add .

---
+++ Assets/Scripts/TransformWindows.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformWindows : MonoBehaviour {
+
+
+    public bool estOuvert = false;
+
+    public float N_PositionX;
+    public float N_PositionY;
+    public float N_PositionZ;
+
+    public float N_RotationX;
+    public float N_RotationY;
+    public float N_RotationZ;
+
+    float A_PositionX = 0.0f;
+    float A_PositionY = 0.0f;
+    float A_PositionZ = 0.0f;
+
+    float A_RotationX = 0.0f;
+    float A_RotationY = 0.0f;
+    float A_RotationZ = 0.0f;
+
+    private void Start()
+    {
+        A_PositionX = transform.position.x;
+        A_PositionY = transform.position.y;
+        A_PositionZ = transform.position.z;
+
+        A_RotationX = transform.eulerAngles.x;
+        A_RotationY = transform.eulerAngles.y;
+        A_RotationZ = transform.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (estOuvert)
+        {
+            transform.position = new Vector3(N_PositionX, N_PositionY, N_PositionZ);
+            transform.eulerAngles = new Vector3(N_RotationX, N_RotationY, N_RotationZ);
+        } else {
+            transform.position = new Vector3(A_PositionX, A_PositionY, A_PositionZ);
+            transform.eulerAngles = new Vector3(A_RotationX, A_PositionY, A_RotationZ);
+        }
+	}
+}
