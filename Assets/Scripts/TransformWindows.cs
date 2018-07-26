3b2ff97ce4fa04108e0b0bb6e347e04120018633
Mbow1 <mbow.morniass@gmail.com>
2018-07-26 16:57:35 +0100

    réussite pour les fenetre juste quelque ajustement et ça ira git add .!

--- Assets/Scripts/TransformWindows.cs
+++ Assets/Scripts/TransformWindows.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class TransformWindows : MonoBehaviour {
 
+    /*
+        public bool estOuvert = false;
 
-    public bool estOuvert = false;
+        public float N_PositionX;
+        public float N_PositionY;
+        public float N_PositionZ;
 
-    public float N_PositionX;
-    public float N_PositionY;
-    public float N_PositionZ;
+        public float N_RotationX;
+        public float N_RotationY;
+        public float N_RotationZ;
 
-    public float N_RotationX;
-    public float N_RotationY;
-    public float N_RotationZ;
+        float A_PositionX = 0.0f;
+        float A_PositionY = 0.0f;
+        float A_PositionZ = 0.0f;
+
+        float A_RotationX = 0.0f;
+        float A_RotationY = 0.0f;
+        float A_RotationZ = 0.0f;
+
+        private void Start()
+        {
+            A_PositionX = transform.position.x;
+            A_PositionY = transform.position.y;
+            A_PositionZ = transform.position.z;
+
+            A_RotationX = transform.eulerAngles.x;
+            A_RotationY = transform.eulerAngles.y;
+            A_RotationZ = transform.eulerAngles.z;
+        }
+
+        // Update is called once per frame
+        void Update () {
+            if (estOuvert)
+            {
+                transform.position = new Vector3(N_PositionX, N_PositionY, N_PositionZ);
+                transform.eulerAngles = new Vector3(N_RotationX, N_RotationY, N_RotationZ);
+            } else {
+                transform.position = new Vector3(A_PositionX, A_PositionY, A_PositionZ);
+                transform.eulerAngles = new Vector3(A_RotationX, A_PositionY, A_RotationZ);
+            }
+        }*/
+
+
+    public bool EstOuvert { get; set; }
+    public bool Gauche;
+    //public bool Gauche { get; set; }
+
 
     float A_PositionX = 0.0f;
     float A_PositionY = 0.0f;
@@ -23,8 +60,10 @@ public class TransformWindows : MonoBehaviour {
     float A_RotationY = 0.0f;
     float A_RotationZ = 0.0f;
 
+
     private void Start()
     {
+
         A_PositionX = transform.position.x;
         A_PositionY = transform.position.y;
         A_PositionZ = transform.position.z;
@@ -32,17 +71,25 @@ public class TransformWindows : MonoBehaviour {
         A_RotationX = transform.eulerAngles.x;
         A_RotationY = transform.eulerAngles.y;
         A_RotationZ = transform.eulerAngles.z;
+        EstOuvert = false;
+        Gauche = false;
     }
 
-    // Update is called once per frame
-    void Update () {
-        if (estOuvert)
+    void Update()
+    {
+        if (EstOuvert && !Gauche)
         {
-            transform.position = new Vector3(N_PositionX, N_PositionY, N_PositionZ);
-            transform.eulerAngles = new Vector3(N_RotationX, N_RotationY, N_RotationZ);
-        } else {
-            transform.position = new Vector3(A_PositionX, A_PositionY, A_PositionZ);
-            transform.eulerAngles = new Vector3(A_RotationX, A_PositionY, A_RotationZ);
+            transform.eulerAngles = new Vector3(A_RotationX, A_RotationY - 45, A_RotationZ);
+            transform.position = new Vector3(A_PositionX + 0.1f, A_PositionY, A_PositionZ - 0.3f);
         }
-	}
+        else if (EstOuvert && Gauche)
+        {
+            transform.eulerAngles = new Vector3(A_RotationX, A_RotationY, A_RotationZ + 45);
+            transform.position = new Vector3(A_PositionX - 0.1f, A_PositionY, A_PositionZ - 0.3f);
+        } else 
+        {
+            transform.eulerAngles = new Vector3(A_RotationX, A_RotationY, A_RotationZ);
+            transform.position = new Vector3(A_PositionX, A_PositionY, A_PositionZ);
+        }
+    }
 }
---
+++ Assets/Scripts/phpAunity.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class FenetreUpdater
+{     
+    bool newetatfenetre = false;
+    int id;
+
+    internal FenetreUpdater(int pid)
+    {
+        id = pid;
+    }
+
+    private IEnumerator MetAJour()
+    {
+        
+        WWW dataFromphp = new WWW("http://127.0.0.1/a/dataWindows.php");
+
+        yield return dataFromphp;
+        string dataFromphpstring = dataFromphp.text;
+
+        //string dataFromphpstring = string.Empty;
+        //DoRequest(out dataFromphpstring);
+        string[] data = dataFromphpstring.Split(';');
+        /*
+        print(getDataValues(data[0], "id :"));
+        print(getDataValues(data[0], "Time :"));
+        print(getDataValues(data[0], "Etat :"));
+        print(getDataValues(data[0], "Numero :"));
+         */
+        for (int i = 0; i <= 18; i++)
+        {
+            if (id.ToString() == getDataValues(data[i], "Numero :"))
+            {
+                if (getDataValues(data[0], "Etat :") == "0")
+                    newetatfenetre = true;
+                if (getDataValues(data[0], "Etat :") == "1")
+                    newetatfenetre = false;
+            }
+        }
+       
+    }
+
+    string getDataValues(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        value = value.Remove(value.IndexOf("|"));
+        return value;
+    }
+
+    internal bool GetEtat()
+    {
+        MetAJour();
+        return newetatfenetre;
+    }
+}
+
+public class phpAunity : MonoBehaviour
+{
+
+
+    public int id;
+    bool newetatfenetre;
+
+    string[] data;
+    // Use this for initialization
+    FenetreUpdater updater;
+
+    void Start()
+    {
+        WWW dataFromphp = new WWW("http://127.0.0.1/a/dataWindows.php");
+
+        while(!dataFromphp.isDone)
+            System.Threading.Thread.Sleep(1);
+        //yield return dataFromphp;
+        string dataFromphpstring = dataFromphp.text;
+        data = dataFromphpstring.Split(';');
+        /*
+        print(getDataValues(data[0], "id :"));
+        print(getDataValues(data[0], "Time :"));
+        print(getDataValues(data[0], "Etat :"));
+        print(getDataValues(data[0], "Numero :"));
+         */
+        for (int i = 0; i<data.Length-1; i++)
+        {
+            if (id.ToString() == getDataValues(data[i], "Numero :"))
+            {
+                if (getDataValues(data[0], "Etat :") == "0")
+                    newetatfenetre = true;
+                if (getDataValues(data[0], "Etat :") == "1")
+                    newetatfenetre = false;
+            }
+        }
+
+    }
+
+    string getDataValues(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        value = value.Remove(value.IndexOf("|"));
+        return value;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // newetatfenetre = updater.GetEtat();
+        Start();
+
+        this.GetComponent<TransformWindows>().EstOuvert = newetatfenetre;
+       
+
+    }
+}
